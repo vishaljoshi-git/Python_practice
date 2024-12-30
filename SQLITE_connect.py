007b23fc94eb418f2dda69220bb9392f311bb8fa
vishaljoshi-git <joshivishal506@gmail.com>
2024-12-30 21:28:16 +0530

    Added full code

--- SQLITE_connect.py
+++ SQLITE_connect.py
@@ -1,2 +1,14 @@
 import sqlite3
 # connect to a database or create new database
+connection  = sqlite3.connect ("test.db")
+ 
+ # create a cursor object to execute command
+cursor = connection.cursor()
+cursor.execute(" CREATE TABLE IF NOT EXISTS task(id INTEGER PRIMARY KEY)")
+
+ # Insert a row
+cursor.execute("INSERT INTO task VALUES (1)")
+connection.commit()
+connection.close()
+
+print("SQL LITE WORKING CORRECTLY")
